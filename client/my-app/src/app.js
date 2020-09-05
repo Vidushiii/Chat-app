a200d70cf63bd5adf9a1b6c8f889f00aa8238d7c
vidushiii <vidutomar19@gmail.com>
2020-09-06 01:33:27 +0530

    commit

---
+++ client/my-app/src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import join from './components/join';
+import chat from './components/chat';
+
+const App = () => (
+<Router>
+    <Route path="/" exact component={join} />
+    <Route path="/chat" component={chat} />
+</Router>
+);
+
+export default App;
+
---
+++ client/my-app/src/components/chat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const chat = () => {
+    return (
+<h1>chat</h1>
+)
+}
+
+export default chat;
